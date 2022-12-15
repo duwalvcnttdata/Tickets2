05da40e7ed91b790b3aea97ca2d413bd461b1b87
Duwal Abel Varillas Castro <dvarillc@emeal.nttdata.com>
2022-12-14 19:53:15 -0500

    Initial Commit

---
+++ Ticket2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ticket2
+//
+//  Created by Duwal Abel Varillas Castro on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
